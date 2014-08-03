0eee2e28df03d5782ae0f6fc107a7313149aaa7c
prabirshrestha <mail@prabir.me>
2014-08-02 21:37:01 -0700

    env: help, version and zeroByte support

--- src/env/env.go
+++ src/env/env.go
@@ -1,14 +1,59 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"code.google.com/p/opts-go"
+	"fmt"
+	"os"
+)
+
+const (
+	version = `env (go coretutils) 0.1
+Packaged by Prabir Shrestha
+Copyright (c) 2014 Prabir Shrestha
+License MIT: <http://opensource.org/licenses/MIT>
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by Prabir Shrestha`
+
+	usage = `Usage: env [OPTION]... [-] [NAME=VALUE]... [COMMAND [ARG]...]
+Set each NAME to VALUE in the environment and run COMMAND.
+
+  -i, --ignore-environment  start with an empty environment
+  -0, --null           end each output line with 0 byte rather than newline
+  -u, --unset=NAME     remove variable from the environment
+      --help     display this help and exit
+      --version  output version information and exit
+
+A mere - implies -i.  If no COMMAND, print the resulting environment.`
 )
 
 func main() {
-    env := os.Environ()
+	opts.Usage = usage
+	showHelp := opts.Flag("", "--help", "Help")
+	showVersion := opts.Flag("", "--version", "Version")
+	zeroByte := opts.Flag("-0", "--null", "ZeroByte")
 
-    for _, envar := range env {
-        fmt.Println(envar)
-    }
+	opts.Parse()
+
+	if *showHelp {
+		fmt.Print(usage)
+		os.Exit(0)
+	}
+
+	if *showVersion {
+		fmt.Print(version)
+		os.Exit(0)
+	}
+
+	env := os.Environ()[2:] // [1] -> working directory, [2] -> exit code; so skip these
+
+	for _, envar := range env {
+		if *zeroByte {
+			fmt.Print(envar)
+			fmt.Print("\x00")
+		} else {
+			fmt.Println(envar)
+		}
+	}
 }
